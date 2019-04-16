d15340013c05f91ea29d8a29d41cbf4cb557e3c3
vblazhnov <vblazhnov@yandex.ru>
2019-04-16 10:28:26 +0300

    ACPKM modes corrected

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # GostMagma
 
-Welcome to your new gem! In this directory, you'll find the files you need to be able to package up your Ruby library into a gem. Put your Ruby code in the file `lib/gost_magma`. To experiment with that code, run `bin/console` for an interactive prompt.
-
-TODO: Delete this and the text above, and describe your gem
+GOST R 34.12/13-2015 (Magma) block cipher algorithms for ECB, CBC, CTR, OFB, CFB, OMAC, CTR-ACPKM and OMAC-ACPKM modes.
 
 ## Installation
 
@@ -22,7 +20,79 @@ Or install it yourself as:
 
 ## Usage
 
-TODO: Write usage instructions here
+```ruby
+require 'gost_magma'
+include GostMagma
+
+BlockSize = Magma::BlockLengthInBytes
+
+# GOST R 34.13-2015 Magma test data
+SelfTestGostMMasterKeyData = [
+  0xff, 0xee, 0xdd, 0xcc, 0xbb, 0xaa, 0x99, 0x88, 
+  0x77, 0x66, 0x55, 0x44, 0x33, 0x22, 0x11, 0x00,
+  0xf0, 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7, 
+  0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe, 0xff
+].pack('C*').freeze
+key = SelfTestGostMMasterKeyData
+
+SelfTestGostMPlainText = [
+  0x92, 0xde, 0xf0, 0x6b, 0x3c, 0x13, 0x0a, 0x59, 
+  0xdb, 0x54, 0xc7, 0x04, 0xf8, 0x18, 0x9d, 0x20,
+  0x4a, 0x98, 0xfb, 0x2e, 0x67, 0xa8, 0x02, 0x4c, 
+  0x89, 0x12, 0x40, 0x9b, 0x17, 0xb5, 0x7e, 0x41,
+].pack('C*').freeze
+plain_text = SelfTestGostMPlainText
+
+# ECB mode
+SelfTestGostMEcbEncText = [
+  0x2b, 0x07, 0x3f, 0x04, 0x94, 0xf3, 0x72, 0xa0, 
+  0xde, 0x70, 0xe7, 0x15, 0xd3, 0x55, 0x6e, 0x48,
+  0x11, 0xd8, 0xd9, 0xe9, 0xea, 0xcf, 0xbc, 0x1e, 
+  0x7c, 0x68, 0x26, 0x09, 0x96, 0xc6, 0x7e, 0xfb
+].pack('C*').freeze
+encrypted_test = SelfTestGostMEcbEncText
+
+encrypted_text = MagmaEcb.new(key).encrypt(plain_text)
+puts "ECB encrypted_text == encrypted_test: #{encrypted_text == encrypted_test}" 
+    
+decrypted_text = MagmaEcb.new(key).decrypt(encrypted_test)
+puts "ECB decrypted_text == plain_text: #{decrypted_text == plain_text}" 
+
+# OMAC mode
+SelfTestGostMMacValue = [
+  0x15, 0x4e, 0x72, 0x10, 0x20, 0x30, 0xc5, 0xbb
+].pack('C*').freeze
+mac_test = SelfTestGostMMacValue
+
+mac = MagmaOmac.new(key, mac_test.length).update(plain_text).final
+puts "OMAC mac == mac_test: #{mac == mac_test}" 
+
+# CTR mode 
+SelfTestGostMCtrSV = [
+  0x12, 0x34, 0x56, 0x78
+].pack('C*').freeze
+iv = SelfTestGostMCtrSV
+
+SelfTestGostMCtrEncText = [
+  0x4e, 0x98, 0x11, 0x0c, 0x97, 0xb7, 0xb9, 0x3c,
+  0x3e, 0x25, 0x0d, 0x93, 0xd6, 0xe8, 0x5d, 0x69,
+  0x13, 0x6d, 0x86, 0x88, 0x07, 0xb2, 0xdb, 0xef,
+  0x56, 0x8e, 0xb6, 0x80, 0xab, 0x52, 0xa1, 0x2d    
+].pack('C*').freeze
+encrypted_test = SelfTestGostMCtrEncText
+
+encrypted_text = MagmaCtr.new(key, iv, BlockSize).encrypt(plain_text)
+puts "CTR encrypted_text == encrypted_test: #{encrypted_text == encrypted_test}"
+
+# CTR multi-part usage    
+text_len = plain_text.length
+ctx = MagmaCtr.new(key, iv, BlockSize)
+decrypted_text = ctx.decrypt(encrypted_test[0...text_len/3]) +
+  ctx.decrypt(encrypted_test[text_len/3..-1])
+puts "CTR decrypted_text == plain_text: #{decrypted_text == plain_text}" 
+```
+
+For other cipher modes see test samples in /test/gost_magma_test.rb please.
 
 ## Development
 
@@ -32,7 +102,7 @@ To install this gem onto your local machine, run `bundle exec rake install`. To 
 
 ## Contributing
 
-Bug reports and pull requests are welcome on GitHub at https://github.com/[USERNAME]/gost_magma.
+Bug reports and pull requests are welcome on GitHub at https://github.com/vblazhnovgit/gost_magma.
 
 ## License
 
--- lib/gost_magma/magma_omac_acpkm.rb
+++ lib/gost_magma/magma_omac_acpkm.rb
@@ -3,7 +3,7 @@ module GostMagma
   class MagmaOmacAcpkm < MagmaCtrAcpkm
 
     def initialize(key, section_N, base_key_change_T, mac_size)
-      puts 'MagmaOmacAcpkm initialize'
+#      puts 'MagmaOmacAcpkm initialize'
       @mac_N = section_N
       @mac_T = base_key_change_T 
       @mac_size = mac_size
@@ -25,12 +25,23 @@ module GostMagma
       @mac_section_counter = 0
       @mac_base_key_counter = 0
       @mac_derived_key_counter = 0
+=begin      
+      printf("N = %d bits\n", @mac_N*8);
+      printf("T = %d bits\n", @mac_T*8);
+      printf("s = %d bits\n", @mac_size*8);
+      printf("=====================================\n");
+      printf("Base key K*[%d]:\n", @mac_base_key_counter+1)
+      Magma::printBytes(@key)
+      printf("=====================================\n");
+      printf("Section %d\n", @mac_section_counter+1)
+      printf("-------------------------------------\n");
+=end      
       deriveKeys
       @mac_derived_key_counter += 1   
     end
     
     def update(data)
-      puts 'MagmaOmacAcpkm update'
+#      puts 'MagmaOmacAcpkm update'
       data_len = data.length
       (0...data_len).each do |k|
         if @mac_lastBlockSize < BlockLengthInBytes then
@@ -44,10 +55,25 @@ module GostMagma
         end  
         if @mac_lastBlockSize == BlockLengthInBytes then
           @mac_block_counter += 1
+=begin          
+		  printf("Input block P[%d]:\n", @mac_block_counter)
+		  Magma::printBytes(@mac_lastBlock)
+		  printf("C[%d]:\n", @mac_block_counter-1)
+		  Magma::printBytes(@mac_C)
+=end      
           (0...BlockLengthInBytes).each do |j|
             @mac_encrBlock[j] = (@mac_lastBlock[j].ord ^ @mac_C[j].ord).chr
           end
+=begin          
+		  printf("P[%d]^C[%d]:\n", @mac_block_counter, @mac_block_counter-1)
+		  Magma::printBytes(@mac_encrBlock)
+		  printf("@mac_C = @ecb.encrypt(@mac_encrBlock)\n")
+=end      
           @mac_C = @ecb.encrypt(@mac_encrBlock)
+=begin          
+		  printf("C[%d]:\n", @mac_block_counter)
+		  Magma::printBytes(@mac_C)
+=end      
           if @mac_byte_counter % @mac_N == 0 then
             if @mac_section_counter >= OMAC_ACPKM_MAX_N then
               puts('Input message is too long for Magma OMAC-ACPKM')
@@ -55,10 +81,21 @@ module GostMagma
             end  
             @mac_section_counter += 1
             @mac_byte_counter = 0
-            if (@mac_derived_key_counter * (KeyLengthInBytes + BlockLengthInBytes)) % @mac_T == 0 then
+=begin            
+        printf("-------------------------------------\n")
+        printf("Section %d\n", @mac_section_counter+1)
+        printf("-------------------------------------\n")
+=end        
+            if (@mac_derived_key_counter * @mac_N) % @mac_T == 0 then
               @mac_base_key_counter += 1
               acpkmCtrKey
             end
+=begin            
+          printf("=====================================\n")
+          printf("Base key K*[%d]:\n", @mac_base_key_counter+1)
+				  Magma::printBytes(@key)
+          printf("=====================================\n")
+=end          
             deriveKeys
             @mac_derived_key_counter += 1
           end
@@ -70,26 +107,53 @@ module GostMagma
     end
     
     def final
-      puts 'MagmaOmacAcpkm final'
+#      puts 'MagmaOmacAcpkm final'
       @mac_lastBlock = @mac_lastBlock[0...@mac_lastBlockSize]
       if (@mac_byte_counter == 0) || (@mac_lastBlockSize > 0) then
         if @mac_lastBlockSize != BlockLengthInBytes then
           @mac_lastBlock = Magma::padd(@mac_lastBlock)
+=begin          
+      printf("Padded input block P[%d]:\n", @mac_block_counter)
+			Magma::printBytes(@mac_lastBlock)
+=end      
         end
       end
+=begin      
+	printf("Input block P[%d]:\n", @mac_block_counter)
+	Magma::printBytes(@mac_lastBlock)
+	printf("C[%d]:\n", @mac_block_counter-1)
+	Magma::printBytes(@mac_C)
+=end  
       (0...BlockLengthInBytes).each do |j|
         @mac_encrBlock[j] = (@mac_lastBlock[j].ord ^ @mac_C[j].ord).chr
       end
+=begin      
+	printf("P[%d]^C[%d]:\n", @mac_block_counter, @mac_block_counter-1)
+	Magma::printBytes(@mac_encrBlock)
+=end  
       if @mac_lastBlockSize != BlockLengthInBytes then
         deriveK2
         kk = @mac_K2
       else
         kk = @mac_K1
       end
+=begin      
+	printf("K':\n")
+	Magma::printBytes(kk)
+=end  
       (0...BlockLengthInBytes).each do |j|
         @mac_encrBlock[j] = (@mac_encrBlock[j].ord ^ kk[j].ord).chr
       end
+=begin      
+	printf("P[%d]^C[%d]^K':\n", @mac_block_counter, @mac_block_counter-1)
+	Magma::printBytes(@mac_encrBlock)
+	printf("mac = @ecb.encrypt(@mac_encrBlock):\n")
+=end  
       mac = @ecb.encrypt(@mac_encrBlock)
+=begin      
+	printf("mac:\n");
+	Magma::printBytes(mac)
+=end      
       result = mac[0...@mac_size].dup
     end
 
@@ -104,14 +168,28 @@ module GostMagma
     ACPKM_CTR_NULL = Magma::zeroBytes(KeyLengthInBytes+BlockLengthInBytes)
 
     def deriveKeys
-      kk = encrypt(ACPKM_CTR_NULL)
+#      puts 'deriveKeys'
+      kk = encrypt(ACPKM_CTR_NULL)      
       # re-initialize ECB context
+=begin      
+	printf("K[%d]:\n", @mac_derived_key_counter+1)
+	Magma::printBytes(kk[0...KeyLengthInBytes])
+=end  
       @ecb = MagmaEcb.new(kk[0...KeyLengthInBytes].dup)
       @mac_K1 = kk[KeyLengthInBytes..-1].dup
+=begin      
+	printf("K1[%d]:\n", @mac_derived_key_counter+1)
+	Magma::printBytes(@mac_K1)
+=end  
     end
 
     def deriveK2
+#      puts 'deriveK2'
       tmp_block = @mac_K1.dup
+=begin      
+	printf("tmp_block:\n");
+	Magma::printBytes(tmp_block) 
+=end  
       Magma::shiftLeftOne(tmp_block)
       if (@mac_K1[0].ord & 0x80) == 0x80 then
         (0...BlockLengthInBytes).each do |i|
@@ -120,6 +198,10 @@ module GostMagma
       else
         @mac_K2 = tmp_block.dup
       end
+=begin      
+	printf("@mac_K2:\n")
+	Magma::printBytes(@mac_K2)
+=end  
     end
 
   end
